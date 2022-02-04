2752c813e21c368ccbcac99fb50ce4e8a314c2c3
mbrinkl <bjbrinkleyyy@yahoo.com>
2022-02-04 15:29:56 -0600

    add sslify

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,13 @@
 import path from 'path';
 import serve from 'koa-static';
+import sslify, { xForwardedProtoResolver as resolver } from 'koa-sslify';
 import { Server, Origins } from 'boardgame.io/server';
 import { DEFAULT_PORT, isProduction } from '../src/config';
 import { SantoriniGame } from '../src/game';
 
 const root = path.join(__dirname, '../build');
 const PORT = Number(process.env.PORT || DEFAULT_PORT);
-const serverURL = isProduction ? 'http://santorini.herokuapp.com/' : 'http://192.168.0.140:3000';
+const serverURL = isProduction ? 'https://santorini.herokuapp.com/' : 'http://192.168.0.140:3000';
 
 const server = Server({
   games: [SantoriniGame],
@@ -19,6 +20,7 @@ const server = Server({
 server.app.use(serve(root));
 
 server.run(PORT, () => {
+  server.app.use(sslify({ resolver }));
   server.app.use(
     (ctx, next) => serve(root)(
       { ...ctx, path: 'index.html' },
