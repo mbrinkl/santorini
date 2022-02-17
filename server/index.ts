6d3c3b3a6f44f2e3ecf920dfd1afc4c58e1f7251
mbrinkl <bjbrinkleyyy@yahoo.com>
2022-02-17 12:34:08 -0600

    add postgres store

--- server/index.ts
+++ server/index.ts
@@ -5,15 +5,32 @@ import sslify, { xForwardedProtoResolver as resolver } from 'koa-sslify';
 import { historyApiFallback } from 'koa2-connect-history-api-fallback';
 import * as Sentry from '@sentry/node';
 import { Server, Origins } from 'boardgame.io/server';
+import { PostgresStore } from 'bgio-postgres';
+import { StorageCache } from '@boardgame.io/storage-cache';
 import { DEFAULT_PORT, isProduction } from '../src/config';
 import { SantoriniGame } from '../src/game';
 
 const root = path.join(__dirname, '../build');
 const PORT = Number(process.env.PORT || DEFAULT_PORT);
 const serverURL = isProduction ? 'https://santorini.herokuapp.com/' : 'http://192.168.0.140:3000';
+const db = new PostgresStore({
+  database: process.env.DB,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  host: process.env.DB_HOST,
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+  logging: false,
+});
 
 const server = Server({
   games: [SantoriniGame],
+  db: new StorageCache(db),
   origins: [
     serverURL,
     Origins.LOCALHOST_IN_DEVELOPMENT,
@@ -41,19 +58,3 @@ server.app.use(
 server.app.use(serve(root));
 
 server.run(PORT);
-
-const day = 24 * 60 * 60 * 1000;
-async function deleteStaleGames() {
-  const weekAgo = Date.now() - 7 * day;
-  // Retrieve matchIDs for matches unchanged for > 1 week.
-  const staleMatchIDs = await server.db.listMatches({
-    where: {
-      updatedBefore: weekAgo,
-    },
-  });
-  // Delete matches
-  for (let i = 0; i < staleMatchIDs.length; i++) {
-    server.db.wipe(staleMatchIDs[i]);
-  }
-}
-setInterval(deleteStaleGames, day);
