b5be164f807650e6e0cc06c6d604a5f779b4cfd5
Ryan Brinkley <ryanbrinkley@yahoo.com>
2022-02-20 10:01:25 -0600

    Added Helios from Island of Misfits

--- src/game/characters/index.ts
+++ src/game/characters/index.ts
@@ -35,6 +35,7 @@ import { Siren } from './Siren';
 import { Hestia } from './Hestia';
 import { Poseidon } from './Poseidon';
 import { LernaeanHydra } from './LernaeanHydra';
+import { Helios } from './Helios'
 
 export const characterList: string[] = [
   'Random',
@@ -119,6 +120,7 @@ export const characterList: string[] = [
 
   // Custom
   'Lernaean Hydra',
+  'Helios',
 ];
 
 export const banList: [string, string][] = [
@@ -168,7 +170,7 @@ export function getCharacterByName(name: string): Character {
     case 'Prometheus': character = Prometheus; break;
     case 'Bia': character = Bia; break;
     case 'Triton': character = Triton; break;
-    case 'Zeus': character = Zeus; break;
+    case 'Zeus': character = ;; break;
     case 'Graeae': character = Graeae; break;
     case 'Heracles': character = Heracles; break;
     case 'Odysseus': character = Odysseus; break;
@@ -191,6 +193,7 @@ export function getCharacterByName(name: string): Character {
     case 'Hestia': character = Hestia; break;
     case 'Poseidon': character = Poseidon; break;
     case 'Lernaean Hydra': character = LernaeanHydra; break;
+    case 'Helios': character = Helios; break;
     default: character = Mortal; break;
   }
 
