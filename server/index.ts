2ecfe9fa649823ce1404be0ec2bc01f36cde9f32
mbrinkl <bjbrinkleyyy@yahoo.com>
2022-09-20 22:39:04 -0500

    migrate database to render

--- server/index.ts
+++ server/index.ts
@@ -1,14 +1,12 @@
 import 'dotenv/config';
 import path from 'path';
 import serve from 'koa-static';
-import sslify, { xForwardedProtoResolver as resolver } from 'koa-sslify';
 import { historyApiFallback } from 'koa2-connect-history-api-fallback';
-import * as Sentry from '@sentry/node';
 import { Server, Origins } from 'boardgame.io/server';
-// import { PostgresStore } from 'bgio-postgres';
+import { PostgresStore } from 'bgio-postgres';
 import { DEFAULT_PORT, isProduction } from '../src/config';
 import { SantoriniGame } from '../src/game';
-// import { ExtendedStorageCache } from './storage';
+import { ExtendedStorageCache } from './storage';
 
 const root = path.join(__dirname, '../build');
 const PORT = Number(process.env.PORT || DEFAULT_PORT);
@@ -18,41 +16,17 @@ const serverURL = isProduction
 
 const server = Server({
   games: [SantoriniGame],
-  // db: new ExtendedStorageCache(
-  //   new PostgresStore({
-  //     database: process.env.DB,
-  //     username: process.env.DB_USER,
-  //     password: process.env.DB_PASS,
-  //     host: process.env.DB_HOST,
-  //     logging: false,
-  //     ...(isProduction && {
-  //       dialect: 'postgres',
-  //       dialectOptions: {
-  //         ssl: {
-  //           require: true,
-  //           rejectUnauthorized: false,
-  //         },
-  //       },
-  //     }),
-  //   }),
-  // ),
+  db: new ExtendedStorageCache(
+    new PostgresStore({
+      database: process.env.DB,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      host: process.env.DB_HOST,
+      logging: false,
+    }),
+  ),
   origins: [serverURL, Origins.LOCALHOST_IN_DEVELOPMENT],
 });
-
-// if (isProduction) {
-//   Sentry.init({ dsn: process.env.SENTRY_DSN });
-
-//   server.app.on('error', (err, ctx) => {
-//     Sentry.withScope((scope) => {
-//       scope.addEventProcessor((event) =>
-//         Sentry.Handlers.parseRequest(event, ctx.request),
-//       );
-//       Sentry.captureException(err);
-//     });
-//   });
-
-//   server.app.use(sslify({ resolver }));
-// }
 
 server.app.use(
   historyApiFallback({
