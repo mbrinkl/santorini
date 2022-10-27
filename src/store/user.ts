873f3bc8bf49b7333fc3dad26660b77d02eb52f4
mbrinkl <bjbrinkleyyy@yahoo.com>
2022-10-27 09:39:30 -0500

    prevent double join lobby

--- src/store/user.ts
+++ src/store/user.ts
@@ -6,10 +6,13 @@ import {
   JoinRoomParams,
   LeaveRoomParams,
 } from '../types/storeTypes';
+import { RootState } from '.';
 
 interface State {
   nickname: string | null;
   activeRoomPlayer: ActiveRoomPlayer | null;
+  isJoining: boolean;
+  currentJoiningRequestID?: string;
 }
 
 const localRoomData = localStorage.getItem(PLAYER_STORAGE_KEY);
@@ -17,12 +20,25 @@ const localRoomData = localStorage.getItem(PLAYER_STORAGE_KEY);
 const initialState: State = {
   nickname: localStorage.getItem(NICKNAME_STORAGE_KEY),
   activeRoomPlayer: localRoomData && JSON.parse(localRoomData),
+  isJoining: false,
+  currentJoiningRequestID: undefined,
 };
 
 export const joinMatchThunk = createAsyncThunk<
-  ActiveRoomPlayer,
-  JoinRoomParams
->('user/joinMatch', async (params) => {
+  ActiveRoomPlayer | null,
+  JoinRoomParams,
+  {
+    state: RootState;
+  }
+>('user/joinMatch', async (params, { getState, requestId }) => {
+  const {
+    user: { isJoining, currentJoiningRequestID },
+  } = getState();
+
+  if (isJoining && requestId !== currentJoiningRequestID) {
+    return null;
+  }
+
   const [playerID, credentials] = await joinMatch(params);
   return {
     matchID: params.matchID,
@@ -69,7 +85,15 @@ export const userSlice = createSlice({
   },
   extraReducers: (builder) => {
     builder
+      .addCase(joinMatchThunk.pending, (state, action) => {
+        if (!state.isJoining) {
+          state.isJoining = true;
+          state.currentJoiningRequestID = action.meta.requestId;
+        }
+      })
       .addCase(joinMatchThunk.fulfilled, (state, action) => {
+        state.isJoining = false;
+        state.currentJoiningRequestID = undefined;
         state.activeRoomPlayer = action.payload;
         localStorage.setItem(
           PLAYER_STORAGE_KEY,
