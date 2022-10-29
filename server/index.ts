e563a25f189169352e13b1b0e9b194afcc093067
mbrinkl <bjbrinkleyyy@yahoo.com>
2022-10-29 12:27:16 -0500

    fallback to in-memory db in dev

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import path from 'path';
+import chalk from 'chalk';
 import serve from 'koa-static';
 import { historyApiFallback } from 'koa2-connect-history-api-fallback';
 import { Server, Origins } from 'boardgame.io/server';
@@ -9,10 +10,11 @@ import { SantoriniGame } from '../src/game';
 import { ExtendedStorageCache } from './storage';
 import { setupServerJobs } from './jobs';
 
-const server = Server({
-  games: [SantoriniGame],
-  db: new ExtendedStorageCache(
-    new PostgresStore(String(process.env.CONNECTION_STRING), {
+const connectionString = process.env.CONNECTION_STRING;
+let db: ExtendedStorageCache | undefined;
+if (connectionString) {
+  db = new ExtendedStorageCache(
+    new PostgresStore(connectionString, {
       logging: false,
       ...(isProduction && {
         dialect: 'postgres',
@@ -24,10 +26,19 @@ const server = Server({
         },
       }),
     }),
-  ),
+  );
+} else if (isProduction) {
+  throw new Error('CONNECTION_STRING missing from environment variables');
+} else {
+  // eslint-disable-next-line no-console
+  console.log(chalk.yellow('Starting server with in-memory database'));
+}
+
+const server = Server({
+  games: [SantoriniGame],
+  db,
   origins: [
-    process.env.RENDER_EXTERNAL_URL || '',
-    Origins.LOCALHOST_IN_DEVELOPMENT,
+    process.env.RENDER_EXTERNAL_URL || Origins.LOCALHOST_IN_DEVELOPMENT,
   ],
 });
 
